a2f070a301159bac4e573dcf6f51f4039983792f
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2021-10-15 23:22:11 +0300

    reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,7 +5,15 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
-function reverse(str) {}
+const str = 'good morning';
+function reverse(str) {
+  const reverse = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    const element = str[i];
+    reverse.push(element);
+  }
+  const reverseString = reverse.join('');
+  return reverseString;
+}
 
 module.exports = reverse;
