2c50cba39fa2502418c42cfa3430bd5d8907c793
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2021-10-19 21:12:23 +0300

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (typeof obj[str[i]] !== 'number') obj[str[i]] = 0;
+    obj[str[i]]++;
+  }
+
+  return Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
+}
 
 module.exports = maxChar;
