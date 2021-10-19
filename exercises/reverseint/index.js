e39974df14039a63e1c52417a5cd552e4eead060
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2021-10-19 19:54:14 +0300

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (typeof n !== 'number') return;
+  let a = Array.from(String(n), Number);
+
+  let b = [];
+  let c = [];
+  for (let i = 0; i < a.length; i++) {
+    b = a.splice(a.length - i - 1, 1, a[i]);
+    c.push(b[0]);
+  }
+
+  if (isNaN(c[c.length - 1])) {
+    c.pop();
+    const result = c.join('');
+    console.log(result * -1);
+    return parseInt(result * -1);
+  } else {
+    const result = c.join('');
+    console.log(result);
+    return parseInt(result);
+  }
+}
 
 module.exports = reverseInt;
