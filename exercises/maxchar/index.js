10a92204ff5713070ba11f6943618118c798453e
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2021-10-20 22:33:16 +0300

    fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,7 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//best way: !important
 function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  //character map:
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  //for loop for object:
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+function maxChar2(str) {
   let obj = {};
   for (let i = 0; i < str.length; i++) {
     if (typeof obj[str[i]] !== 'number') obj[str[i]] = 0;
