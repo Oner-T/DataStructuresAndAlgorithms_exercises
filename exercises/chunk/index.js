dc55ce2c8a358b926d972126d3d562ac5bd66032
Öner Taylan Öztürk <onertaylanozturk@gmail.com>
2021-11-05 22:52:29 +0300

    chunk alternatives

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,8 +8,7 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
-function chunk(arr, size) {
+function chunk2(arr, size) {
   let arr2 = [];
   for (let i = 0; i < arr.length; i++) {
     let arr3 = [];
@@ -23,6 +22,34 @@ function chunk(arr, size) {
   }
   arr2 = arr2.filter((e) => e.length);
   return arr2;
+}
+
+function chunk(arr, size) {
+  const chunked = [];
+
+  for (let element of arr) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+
+
+function chunk3(arr, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
 }
 
 module.exports = chunk;
